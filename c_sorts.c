56173bc74dbaf9284014fb0a0e77d6dc33dad78d
badroot <baadroot@gmail.com>
2018-03-21 15:57:09 -0400

    Initial build commit for CLion

---
+++ c_sorts.c
@@ -0,0 +1,10 @@
+//
+// Created by Red on 3/21/2018.
+//
+
+//#include "c_sorts.h"
+#include <stdio.h>
+
+void hello(void) {
+    printf("Hello, World!\n");
+}
---
+++ c_sorts.h
@@ -0,0 +1,10 @@
+//
+// Created by Red on 3/21/2018.
+//
+
+#ifndef PROJECT_C_SORTS_H
+#define PROJECT_C_SORTS_H
+
+void hello(void);
+
+#endif //PROJECT_C_SORTS_H
